23175a18c58726ff0cbff185cdcc7e62736bb97e
Alina <alina@onmaster.ru>
2020-12-03 17:07:46 +0300

    Верстка шапки

--- source/js/script.js
+++ source/js/script.js
@@ -1,21 +1,10 @@
-/*let toggle = document.querySelector(".main-nav__toggle");
-let menu = document.querySelector(".main-nav__menu");
-let menuButtons = document.querySelectorAll(".site-nav__item > a");
+let toggle = document.querySelector(".main-nav__toggle");
+let nav = document.querySelector(".main-nav__wrap");
 
 toggle.onclick = function () {
   toggle.classList.toggle("main-nav__toggle--close");
-  menu.classList.toggle("main-nav__menu--vertical");
+  nav.classList.toggle("main-nav__wrap--opened");
 };
-
-
-for (const button of menuButtons) {
-  button.addEventListener('click', function(event) {
-    if(window.innerWidth < 992 ) {
-      toggle.classList.toggle("main-nav__toggle--close");
-      menu.classList.toggle("main-nav__menu--vertical");
-    }
-  })
-}*/
 
 
 /* Карусели */
