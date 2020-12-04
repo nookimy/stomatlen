1e32716fc3ac62ebef93aaea6f6448c3d23077fb
Alina <alina@onmaster.ru>
2020-12-04 12:38:27 +0300

    Кнопка Версия для слабовидящих

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,16 @@
 let toggle = document.querySelector(".main-nav__toggle");
 let nav = document.querySelector(".main-nav__wrap");
+let visuallyImpairedBtn = document.querySelector(".visually-impaired__btn");
 
+/*Основное меню*/
 toggle.onclick = function () {
   toggle.classList.toggle("main-nav__toggle--close");
   nav.classList.toggle("main-nav__wrap--opened");
+};
+
+/*Кнопка переключения версии для слабовидящих*/
+visuallyImpairedBtn.onclick = function () {
+  visuallyImpairedBtn.classList.toggle("visually-impaired__btn--eye-crossed");
 };
 
 
